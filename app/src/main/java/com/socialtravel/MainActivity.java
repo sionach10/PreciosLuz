55ba58d6ce04e4523e8e7359adf5b1561ce4bf2a
sionach10 <criptostudios@gmail.com>
2022-06-11 19:19:06 +0200

    Pantalla de registro.

--- app/src/main/java/com/socialtravel/MainActivity.java
+++ app/src/main/java/com/socialtravel/MainActivity.java
@@ -2,13 +2,26 @@ package com.socialtravel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mTextViewRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewRegister = findViewById(R.id.textViewRegister);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class); //Esto me lleva a la pantalla de registro cuando pulso el botón desde la pantalla de Login.
+                startActivity(intent);
+            }
+        });
     }
 }
