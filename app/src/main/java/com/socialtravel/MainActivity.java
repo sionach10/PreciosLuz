7a54a085f5e98490ec1440ca960a9e48e7d1a6b2
sionach10 <criptostudios@gmail.com>
2022-07-21 21:39:19 +0200

    Avanzado en Google SingIn pero esta deprecado.

--- app/src/main/java/com/socialtravel/MainActivity.java
+++ app/src/main/java/com/socialtravel/MainActivity.java
@@ -1,5 +1,6 @@
 package com.socialtravel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
     TextInputEditText mTextInputEmail;
     TextInputEditText mTextInputPassword;
     Button mButtonLogin;
+    FirebaseAuth mAuth;
+    SignInButton mButtonGoogle;
+    private GoogleSignInClient mGoogleSignInClient;
+    private final int REQUEST_CODE_GOOGLE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +41,22 @@ public class MainActivity extends AppCompatActivity {
         mTextInputEmail = findViewById(R.id.textInputEmail);
         mTextInputPassword = findViewById(R.id.textInputPassword);
         mButtonLogin = findViewById(R.id.btnLogin);
+        mButtonGoogle = findViewById(R.id.btnLoginGoogle);
+        mAuth = FirebaseAuth.getInstance();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mButtonGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInGoogle();
+            }
+        });
 
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,11 +74,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void signInGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, REQUEST_CODE_GOOGLE);
+    }
+
     private void login()
     {
         String email = mTextInputEmail.getText().toString();
         String password = mTextInputPassword.getText().toString();
-
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);//Se le pasa pantalla origen y pantalla destino.
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "El email o la contraseña no son correctos.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
         Log.d("Campo", "email: "+ email);
         Log.d("Campo", "Password: "+ password);
     }
