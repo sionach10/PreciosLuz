d326723a621af15b91103294c75d0d37f75a4437
sionach10 <criptostudios@gmail.com>
2022-08-30 12:32:21 +0200

    Navigation bar bottom. Creado el menu inferior del home_activity.

--- app/src/main/java/com/socialtravel/activities/HomeActivity.java
+++ app/src/main/java/com/socialtravel/activities/HomeActivity.java
@@ -1,16 +1,59 @@
 package com.socialtravel.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.socialtravel.R;
+import com.socialtravel.databinding.ActivityHomeBinding;
+import com.socialtravel.databinding.ActivityMainBinding;
+import com.socialtravel.fragments.ChatsFragment;
+import com.socialtravel.fragments.FiltersFragment;
+import com.socialtravel.fragments.HomeFragment;
+import com.socialtravel.fragments.ProfileFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater()); //Enlaza un objeto de java con el objeto del .xml de layout.
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.itemHome:
+                    openFragment(new HomeFragment());
+                    return true;
+                case R.id.itemFilters:
+                    openFragment(new FiltersFragment());
+                    return true;
+                case R.id.itemChat:
+                    openFragment(new ChatsFragment());
+                    return true;
+                case R.id.itemProfile:
+                    openFragment(new ProfileFragment());
+                    return true;
+            }
+
+            return true;
+        });
+
+        openFragment(new HomeFragment());
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
