dd240216a43c8040a55918dc5b8ce9a0238cc370
sionach10 <criptostudios@gmail.com>
2022-09-10 15:09:27 +0200

    Sección 4. Video 29 done. Acceso a galeria y upload de imagenes en la app. Pendiente guadar en firebase en el proximo video.

--- app/src/main/java/com/socialtravel/activities/PostActivity.java
+++ app/src/main/java/com/socialtravel/activities/PostActivity.java
@@ -1,16 +1,80 @@
 package com.socialtravel.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.socialtravel.R;
+import com.socialtravel.utils.FileUtil;
+
+import java.io.File;
+import java.io.IOException;
 
 public class PostActivity extends AppCompatActivity {
+
+    ImageView mImageViewPost1;
+    ImageView mImageViewPost2;
+    File mImageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mImageViewPost1 = findViewById(R.id.imageViewPost1);
+
+        mImageViewPost1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
     }
+
+    private void openGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        activityResultLauncher.launch(galleryIntent);
+    }
+
+    ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult activityResult) {
+                            int requestCode = activityResult.getResultCode();
+                            Intent data = activityResult.getData();
+
+                            //Result returned from launching the Intent
+                            if(requestCode == RESULT_OK) {
+                                try {
+                                    mImageFile = FileUtil.from(PostActivity.this, data.getData());//Nos transforma la URI de la imagen en el archivo (Creo).
+                                    mImageViewPost1.setImageBitmap(BitmapFactory.decodeFile(mImageFile.getAbsolutePath()));
+
+                                }catch(Exception e) {
+                                    Log.w("ERROR","Gallery Intent failed, error: ", e);
+                                    Toast.makeText(PostActivity.this, "Error al cargar la imagen", Toast.LENGTH_SHORT).show();
+                                    // ...
+                                }
+                            }
+
+                        }
+                    }
+            );
+
 }
