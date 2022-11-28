eaeb4e51e08b20b39bbcc7b3f348be7fa11cccaa
sionach10 <criptostudios@gmail.com>
2022-11-28 14:49:54 +0100

    Sección 5 video 48. OK. Mostrar información en el detalle de la publicación.

--- app/src/main/java/com/socialtravel/activities/PostDetailActivity.java
+++ app/src/main/java/com/socialtravel/activities/PostDetailActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -14,9 +17,14 @@ import com.socialtravel.R;
 import com.socialtravel.adapters.SliderAdapter;
 import com.socialtravel.models.SliderItem;
 import com.socialtravel.providers.PostProvider;
+import com.socialtravel.providers.UserProvider;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class PostDetailActivity extends AppCompatActivity {
 
@@ -24,22 +32,45 @@ public class PostDetailActivity extends AppCompatActivity {
     SliderAdapter mSliderAdapter;
     List<SliderItem> mSliderItems = new ArrayList<>();
     PostProvider mPostProvider;
+    UserProvider mUserProvider;
     String mExtraPostId;
+
+    TextView mTextViewTitle;
+    TextView mTextViewDescription;
+    TextView mTextViewUsername;
+    TextView mTextViewPhone;
+    TextView mTextViewNameCategory;
+    ImageView mImageViewCategory;
+    CircleImageView mCircleImageViewProfile;
+    Button mButtonShowProfile;
+
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_detail);
-        mPostProvider = new PostProvider();
 
+        mSliderView = findViewById(R.id.imageSlider);
+        mTextViewTitle = findViewById(R.id.textViewTitle);
+        mTextViewDescription = findViewById(R.id.textViewDescription);
+        mTextViewUsername = findViewById(R.id.textViewUsername);
+        mTextViewPhone = findViewById(R.id.textViewPhone);
+        mTextViewNameCategory = findViewById(R.id.textViewNameCategory);
+        mImageViewCategory = findViewById(R.id.imageViewCategory);
+        mCircleImageViewProfile = findViewById(R.id.circleImageProfile);
+        mButtonShowProfile = findViewById(R.id.btnShowProfile);
+
+        mPostProvider = new PostProvider();
+        mUserProvider = new UserProvider();
         mExtraPostId = getIntent().getStringExtra("id");
 
         getPost();
     }
 
     private void instanceSlider() {
-        mSliderView = findViewById(R.id.imageSlider);
         mSliderAdapter = new SliderAdapter(PostDetailActivity.this, mSliderItems);
         mSliderView.setSliderAdapter(mSliderAdapter);
         mSliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //Forma de pasar los bullets de la animación.
@@ -62,15 +93,76 @@ public class PostDetailActivity extends AppCompatActivity {
                         item.setImageURL(image1);
                         mSliderItems.add(item);
                     }
-                    if(documentSnapshot.contains("image2")){
+                    if(documentSnapshot.contains("image2")) {
                         String image2 = documentSnapshot.getString("image2");
                         SliderItem item = new SliderItem();
                         item.setImageURL(image2);
                         mSliderItems.add(item);
                     }
+                    if(documentSnapshot.contains("title")) {
+                        String title = documentSnapshot.getString("title");
+                        mTextViewTitle.setText(title.toUpperCase());
+                    }
+                    if(documentSnapshot.contains("description")) {
+                        String description = documentSnapshot.getString("description");
+                        mTextViewDescription.setText(description);
+                    }
+                    if(documentSnapshot.contains("category")) {
+                        String category = documentSnapshot.getString("category");
+                        mTextViewNameCategory.setText(category);
+
+                        switch (category) {
+                            case "PS4":
+                                mImageViewCategory.setImageResource(R.drawable.icon_ps4);
+                                break;
+                            case "PC":
+                                mImageViewCategory.setImageResource(R.drawable.icon_pc);
+                                break;
+                            case "NINTENDO":
+                                mImageViewCategory.setImageResource(R.drawable.icon_nintendo);
+                                break;
+                            case "XBOX":
+                                mImageViewCategory.setImageResource(R.drawable.icon_xbox);
+                                break;
+                        }
+                    }
+                    if(documentSnapshot.contains("idUser")) {
+                        String idUser = documentSnapshot.getString("idUser");
+                        getUserInfo(idUser);
+                    }
+
+
                     instanceSlider();
                 }
             }
         });
     }
+
+    private void getUserInfo(String idUser) {
+        mUserProvider.getUser(idUser).addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists()) {
+                    if(documentSnapshot.contains("username")) {
+                        String username = documentSnapshot.getString("username");
+                        mTextViewUsername.setText(username);
+                    }
+                }
+                if(documentSnapshot.exists()) {
+                    if(documentSnapshot.contains("phone")) {
+                        String phone = documentSnapshot.getString("phone");
+                        mTextViewPhone.setText(phone);
+                    }
+                }
+                if(documentSnapshot.exists()) {
+                    if(documentSnapshot.contains("image_profile")) {
+                        String image_profile = documentSnapshot.getString("image_profile");
+                        Picasso.with(PostDetailActivity.this).load(image_profile).into(mCircleImageViewProfile);
+                    }
+                }
+            }
+        });
+    }
+
+
 }
