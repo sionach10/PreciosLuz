124dec187faa9fd9da00df17ca85e9281c0067f9
sionach10 <criptostudios@gmail.com>
2023-02-18 15:03:21 +0100

    Llamada a la API de ESIOS OK. Pendiente parsear el JSON que devuelve ESIOS.

--- app/src/main/java/com/precioLuz/fragments/PricesFragment.java
+++ app/src/main/java/com/precioLuz/fragments/PricesFragment.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
 import android.widget.ImageButton;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -19,12 +18,12 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.type.Date;
 import com.precioLuz.R;
 import com.precioLuz.adapters.PricesAdapter;
 import com.precioLuz.models.PreciosJSON;
@@ -33,11 +32,14 @@ import com.precioLuz.utils.JsonPricesParser;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 
 
 public class PricesFragment extends Fragment {
@@ -54,15 +56,16 @@ public class PricesFragment extends Fragment {
     PricesAdapter mAdapter;
     ImageButton btnCalendar;
 
-    private Calendar mCalendar = Calendar.getInstance();
-    private int day = mCalendar.get(Calendar.DAY_OF_MONTH);
-    private int month = mCalendar.get(Calendar.MONTH);
-    private int year = mCalendar.get(Calendar.YEAR);
-
+    private final Calendar mCalendar = Calendar.getInstance();
+    private final int day = mCalendar.get(Calendar.DAY_OF_MONTH);
+    private final int month = mCalendar.get(Calendar.MONTH); //Los meses los devuelve de 0 a 11.
+    private final int year = mCalendar.get(Calendar.YEAR);
+    String [] fechasBusqueda = new String[2];
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         setHasOptionsMenu(true);
         mView = inflater.inflate(R.layout.fragment_prices, container, false);
@@ -70,7 +73,6 @@ public class PricesFragment extends Fragment {
         listaItemsPrecios= mView.findViewById(R.id.listaItemsPrecios);
         fecha = mView.findViewById(R.id.date);
         btnCalendar = mView.findViewById(R.id.btnCalendar);
-
 
         return mView;
     }
@@ -80,24 +82,52 @@ public class PricesFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        leerWS();
+        //leerWSESIOS();
+
         btnCalendar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
                     @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        day = dayOfMonth;
-                        Log.w("Estoy aqui: ", String.valueOf(day));
-                        //Meter leerWS con la nueva fecha.
+                    public void onDateSet(DatePicker view, int _year, int _month, int _dayOfMonth) {
+                        try {
+                            fechasBusqueda = obtenerFechasFromDatePicker(_dayOfMonth, _month+1, _year);//El datePicker usa los meses de 0 a 11.
+
+                            leerWSESIOS(fechasBusqueda);
+
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
                     }
-                }, year, month, day);
+                }, year, month, day); //Variables para inicializar el calendario en el día de hoy.
                 datePickerDialog.show();
             }
         });
+    }
 
-        leerWS();
+    private String[] obtenerFechasFromDatePicker(int dayOfMonth, int month, int year) throws ParseException {
 
+        String [] _fechasBusqueda = new String[2];
+
+        String _date = String.valueOf(dayOfMonth) + "/" + String.valueOf(month) + "/" + String.valueOf(year);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        Calendar calendar = Calendar.getInstance();
+        try{
+            calendar.setTime(sdf.parse(_date));
+            _date = sdf.format(calendar.getTime());
+        }
+        catch (ParseException e){
+            e.printStackTrace();
+        }
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        String _dateNext = sdf.format(calendar.getTime());
+
+        _fechasBusqueda[0] = _date;
+        _fechasBusqueda[1] = _dateNext;
+        return _fechasBusqueda;
     }
 
 
@@ -134,6 +164,49 @@ public class PricesFragment extends Fragment {
             }
         });
         Volley.newRequestQueue(requireContext()).add(postRequest);
+    }
+
+    private void leerWSESIOS(String [] _fechasBusqueda) {
+
+        String url = "https://api.esios.ree.es/indicators/10391?geo_ids[]=8741&start_date="+_fechasBusqueda[0]+"&end_date="+_fechasBusqueda[1];
+        StringRequest getRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);//Recibimos el JSON.
+
+                    //Lo parseamos a un objeto del tipo PreciosJSON.
+                    PreciosJSON[] preciosJSON = JsonPricesParser.obtenerJsonHoras(jsonObject);
+
+                    //Añadimos los preciosJSON a una lista.
+                    mList.addAll(Arrays.asList(preciosJSON));
+
+                    //Modificamos campo fecha.
+                    fecha.setText(preciosJSON[0].getDate());
+
+                    //Creamos el adaptador con los datos de la lista.
+                    mAdapter = new PricesAdapter(requireContext(),R.layout.item_lista_precios, mList);
+
+                    listaItemsPrecios.setAdapter(mAdapter);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("ResponseError: ", String.valueOf(error));
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<String, String>();
+                headers.put("Authorization", "Token token=\"f8b7eac4ecd150b5e83f2e2f8c218064c8afb9e33eadcd077eef7b243f69bd46\"");
+                return headers;
+            }
+        };
+        Volley.newRequestQueue(requireContext()).add(getRequest);
     }
 
 
