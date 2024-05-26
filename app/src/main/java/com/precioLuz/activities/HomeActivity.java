0851678c400afa234b800c733d7f77676429586e
sionach10 <criptostudios@gmail.com>
2024-05-26 20:24:32 +0200

    - Añadido deslizamiento entre fragments.

--- app/src/main/java/com/precioLuz/activities/HomeActivity.java
+++ app/src/main/java/com/precioLuz/activities/HomeActivity.java
@@ -3,12 +3,12 @@ package com.precioLuz.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.precioLuz.R;
 import com.precioLuz.databinding.ActivityHomeBinding;
 import com.precioLuz.fragments.ChartsFragment;
@@ -24,55 +24,83 @@ public class HomeActivity extends AppCompatActivity {
     TokenProvider mTokenProvider;
     AuthProvider mAuthProvider;
     UserProvider mUserProvider;
-
+    private ViewPager2 viewPager;
+    private FragmentStateAdapter pagerAdapter;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityHomeBinding.inflate(getLayoutInflater()); //Enlaza un objeto de java con el objeto del .xml de layout.
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+        viewPager = binding.viewPager;
+        bottomNavigationView = binding.bottomNavigation;
+
+        // Configura el adaptador del ViewPager2
+        pagerAdapter = new ScreenSlidePagerAdapter(this);
+        viewPager.setAdapter(pagerAdapter);
+
+        // Listener para el ViewPager2
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position) {
+                    case 0:
+                        bottomNavigationView.setSelectedItemId(R.id.itemHome);
+                        break;
+                    case 1:
+                        bottomNavigationView.setSelectedItemId(R.id.itemHourDetail);
+                        break;
+                }
+            }
+        });
+
+        // Listener para el BottomNavigationView
+        bottomNavigationView.setOnItemSelectedListener(item -> {
             switch (item.getItemId()) {
                 case R.id.itemHome:
-                    openFragment(new ChartsFragment());
+                    viewPager.setCurrentItem(0);
                     return true;
-                case R.id.itemHistory:
-                    openFragment(new PricesFragment());
+                case R.id.itemHourDetail:
+                    viewPager.setCurrentItem(1);
                     return true;
             }
-
-            return true;
+            return false;
         });
 
         mTokenProvider = new TokenProvider();
         mAuthProvider = new AuthProvider();
         mUserProvider = new UserProvider();
 
-        openFragment(new ChartsFragment());
         createToken();
     }
 
-    @Override
-    protected void onStart() { //Metodos del ciclo de vida de la app.
-        super.onStart();
-    }
+    private class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+        public ScreenSlidePagerAdapter(FragmentActivity fa) {
+            super(fa);
+        }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
+        @Override
+        public Fragment createFragment(int position) {
+            switch (position) {
+                case 0:
+                    return new ChartsFragment();
+                case 1:
+                    return new PricesFragment();
+                default:
+                    return new ChartsFragment();
+            }
+        }
 
-    public void openFragment(Fragment fragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        @Override
+        public int getItemCount() {
+            return 2; // Número de fragmentos
+        }
     }
 
     private void createToken() {
         mTokenProvider.create(mAuthProvider.getUid());
     }
-
-
 }
